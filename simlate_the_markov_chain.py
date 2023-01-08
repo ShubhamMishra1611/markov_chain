367cafa4314383a15456afcecc59f8ab4eb50e6d
Shubham Mishra <98794040+ShubhamMishra1611@users.noreply.github.com>
2023-01-08 22:59:54 +0530

    Update simlate_the_markov_chain.py

--- simlate_the_markov_chain.py
+++ simlate_the_markov_chain.py
@@ -13,7 +13,7 @@ class Markov_chain:
             like
         '''
         self.states={}
-        self.t_mat=t_mat
+        self.t_mat = t_mat
         for i in range(len(states)):
             if not states[i] in self.states:
                 self.states[i]=states[i]
@@ -25,14 +25,14 @@ class Markov_chain:
 
 
     def calculating_stationary_states(self):
-        w,v1,v2=eig(self.t_mat,left=True)
-        k=[v1[0][i] for i in range(len(v1))]
+        w,v1,v2 = eig(self.t_mat,left=True)
+        k = [v1[0][i] for i in range(len(v1))]
         return k
 
     def simulation(self,initial_state=0,n_steps:int=10):
-        prev_state=initial_state
+        prev_state = initial_state
         for i in range(n_steps):
-            if i%10==0 and i>10:
+            if i%10 == 0 and i > 10:
                 print("")
             current_state=np.random.choice(list(self.states.keys()),p=self.t_mat[prev_state])
             print(self.states[current_state],end="---->")
